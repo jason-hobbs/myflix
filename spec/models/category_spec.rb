5ea368d0833026d937803fa6a5653c2d3debd0fb
Jason Hobbs <grimmstede@feemon.com>
2015-08-20 14:46:36 -0500

    change test to many videos

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -7,14 +7,16 @@ describe Category do
     expect(Category.first).to eq(cat)
   end
 
-  it "has videos" do
+  it "has many videos" do
     cat = Category.new(name: "Comedy")
     cat.save
     cat = Category.first
     video = Video.new(title: "Monk", desc: "A great video!")
     video.category = cat
     video.save
-    vid = Video.first
-    expect(cat.videos.count).to eq(1)
+    video2 = Video.new(title: "Family Guy", desc: "A great video!")
+    video2.category = cat
+    video2.save
+    expect(cat.videos.count).to eq(2)
   end
 end
