1ae85e4cc60f7ba9521e2c8d3822c81550f45124
Jason Hobbs <grimmstede@feemon.com>
2015-08-20 15:15:14 -0500

    added shoulda-matchers gem

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,22 +1,23 @@
 require 'rails_helper'
 
 describe Category do
-  it "saves itself" do
-    cat = Category.new(name: "Comedy")
-    cat.save
-    expect(Category.first).to eq(cat)
-  end
+  it {should have_many(:videos)}
+#  it "saves itself" do
+#    cat = Category.new(name: "Comedy")
+#    cat.save
+#    expect(Category.first).to eq(cat)
+#  end
 
-  it "has many videos" do
-    cat = Category.new(name: "Comedy")
-    cat.save
-    cat = Category.first
-    video = Video.new(title: "Monk", desc: "A great video!")
-    video.category = cat
-    video.save
-    video2 = Video.new(title: "Family Guy", desc: "A great video!")
-    video2.category = cat
-    video2.save
-    expect(cat.videos.count).to eq(2)
-  end
+#  it "has many videos" do
+#    cat = Category.new(name: "Comedy")
+#    cat.save
+#    cat = Category.first
+#    video = Video.new(title: "Monk", desc: "A great video!")
+#    video.category = cat
+#    video.save
+#    video2 = Video.new(title: "Family Guy", desc: "A great video!")
+#    video2.category = cat
+#    video2.save
+#    expect(cat.videos.count).to eq(2)
+#  end
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,29 +1,33 @@
 require 'rails_helper'
 
 describe Video do
-  it "saves itself" do
-    video = Video.new(title: "Monk", desc: "A great video!")
-    video.save
-    expect(Video.first).to eq(video)
-  end
+  it {should belong_to(:category)}
+  it {should validate_presence_of(:title)}
+  it {should validate_presence_of(:desc)}
 
-  it "has a category" do
-    cat = Category.new(name: "Comedy")
-    cat.save
-    video = Video.new(title: "Monk", desc: "A great video!")
-    video.category = cat
-    video.save
-    vid = Video.first
-    expect(vid.category.name).to eq(video.category.name)
-  end
+#  it "saves itself" do
+#    video = Video.new(title: "Monk", desc: "A great video!")
+#    video.save
+#    expect(Video.first).to eq(video)
+#  end
 
-  it "does not save without a title" do
-    video = Video.create(desc: "A great video!")
-    expect(Video.count).to eq(0)
-  end
+#  it "has a category" do
+#    cat = Category.new(name: "Comedy")
+#    cat.save
+#    video = Video.new(title: "Monk", desc: "A great video!")
+#    video.category = cat
+#    video.save
+#    vid = Video.first
+#    expect(vid.category.name).to eq(video.category.name)
+#  end
 
-  it "does not save without a description" do
-    video = Video.create(title: "Monk")
-    expect(Video.count).to eq(0)
-  end
+#  it "does not save without a title" do
+#    video = Video.create(desc: "A great video!")
+#    expect(Video.count).to eq(0)
+#  end
+
+#  it "does not save without a description" do
+#    video = Video.create(title: "Monk")
+#    expect(Video.count).to eq(0)
+#  end
 end
