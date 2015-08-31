4357237ebc36b4037a28402a884579208ed3c41d
Jason Hobbs <grimmstede@feemon.com>
2015-08-30 22:20:15 -0500

    navbar search

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
   get 'home' => 'videos#index'
   root "videos#index"
-  resources :videos, only: [:show, :index]
+  resources :videos, only: [:show, :index] do
+    collection do
+      post :search, to: "videos#search"
+    end
+  end
 end
