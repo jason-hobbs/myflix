02fa898aeb586006de5bb61053d80d6afd852c03
Jason Hobbs <grimmstede@feemon.com>
2015-09-14 11:12:32 -0500

    added user auth

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
   get 'register' => 'users#new'
   get 'sign_in' => 'sessions#new'
+  get 'sign_out' => 'sessions#destroy'
   get 'home' => 'videos#index'
   resources :users, only: [:create]
+  resources :sessions, only: [:create]
   root "pages#front"
   resources :videos, only: [:show, :index] do
     collection do
